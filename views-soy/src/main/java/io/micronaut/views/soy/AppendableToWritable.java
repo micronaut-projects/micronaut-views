49656f785485fa2689b3141ee6a78992d89e2f30
Sam Gammon <sam@bloombox.io>
2019-09-03 19:01:53 -0700

    Cleanup docstrings, copyright headers

--- views-soy/src/main/java/io/micronaut/views/soy/AppendableToWritable.java
+++ views-soy/src/main/java/io/micronaut/views/soy/AppendableToWritable.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.micronaut.views.soy;
 
 import com.google.template.soy.jbcsrc.api.AdvisingAppendable;
@@ -9,24 +25,27 @@ import java.io.Writer;
 
 /**
  * Adapts {@link Appendable} to {@link Writable} for use when rendering Soy templates.
+ *
+ * @author Sam Gammon (sam@bloombox.io)
+ * @since 1.3.0
  */
 public class AppendableToWritable implements Writable, Appendable, AdvisingAppendable {
   private final StringBuilder builder = new StringBuilder();
 
   @Override
-  public AdvisingAppendable append(CharSequence charSequence) throws IOException {
+  public AdvisingAppendable append(CharSequence charSequence) {
     builder.append(charSequence);
     return this;
   }
 
   @Override
-  public AdvisingAppendable append(CharSequence charSequence, int i, int i1) throws IOException {
+  public AdvisingAppendable append(CharSequence charSequence, int i, int i1) {
     builder.append(charSequence, i, i1);
     return this;
   }
 
   @Override
-  public AdvisingAppendable append(char c) throws IOException {
+  public AdvisingAppendable append(char c) {
     builder.append(c);
     return this;
   }
--- views-soy/src/main/java/io/micronaut/views/soy/SoyFileSetProvider.java
+++ views-soy/src/main/java/io/micronaut/views/soy/SoyFileSetProvider.java
@@ -1,5 +1,20 @@
-package io.micronaut.views.soy;
+/*
+ * Copyright 2017-2019 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package io.micronaut.views.soy;
 
 import com.google.template.soy.SoyFileSet;
 import com.google.template.soy.jbcsrc.api.SoySauce;
@@ -11,7 +26,7 @@ import javax.annotation.Nullable;
 /**
  * Interface via DI to acquire a {@link SoyFileSet}.
  *
- * @author Sam Gammon
+ * @author Sam Gammon (sam@bloombox.io)
  * @since 1.3.0
  */
 public interface SoyFileSetProvider {
