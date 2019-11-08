d2516d034a5656ec4de44001a5904c0df7f11909
niravassar <assarn@objectcomputing.com>
2019-11-08 08:47:02 -0600

    java doc fixes

--- views-thymeleaf/src/main/java/io/micronaut/views/thymeleaf/MicronautThymeMessageResolver.java
+++ views-thymeleaf/src/main/java/io/micronaut/views/thymeleaf/MicronautThymeMessageResolver.java
@@ -9,30 +9,41 @@ import org.thymeleaf.util.Validate;
 import javax.inject.Singleton;
 import java.util.Optional;
 
+/**
+ * Resolves messages with the Micronaut MessageSource. Allows Thymeleaf templates to use
+ * micronaut message properties.
+ *
+ * @author Nirav Assar
+ */
 @Singleton
 public class MicronautThymeMessageResolver extends AbstractMessageResolver {
 
     private final StandardMessageResolver standardMessageResolver;
     private MessageSource messageSource;
 
+    /**
+     * Constructor which will initialize teh thymeleaf standard message resolver, and also the message source for
+     * Micronaut.
+     *
+     * @param messageSource Micronaut message source
+     */
     public MicronautThymeMessageResolver(MessageSource messageSource) {
         super();
         this.standardMessageResolver = new StandardMessageResolver();
         this.messageSource = messageSource;
     }
 
+    @Override
     public final String resolveMessage(
             final ITemplateContext context, final Class<?> origin, final String key, final Object[] messageParameters) {
 
         Validate.notNull(key, "Message key cannot be null");
         Optional<String> value = this.messageSource.getMessage(key, MessageSource.MessageContext.DEFAULT);
-        if(value.isPresent()) {
-            return value.get();
-        }
+        return value.orElse(null);
 
-        return null;
     }
 
+    @Override
     public String createAbsentMessageRepresentation(
             final ITemplateContext context, final Class<?> origin, final String key, final Object[] messageParameters) {
         return this.standardMessageResolver.createAbsentMessageRepresentation(context, origin, key, messageParameters);
