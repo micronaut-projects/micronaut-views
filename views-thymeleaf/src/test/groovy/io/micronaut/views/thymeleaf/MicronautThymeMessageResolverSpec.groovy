8dce2c6bd7a51d3010214fe88e4679028360102f
niravassar <assarn@objectcomputing.com>
2019-11-08 10:53:54 -0600

    documentation

--- views-thymeleaf/src/test/groovy/io/micronaut/views/thymeleaf/MicronautThymeMessageResolverSpec.groovy
+++ views-thymeleaf/src/test/groovy/io/micronaut/views/thymeleaf/MicronautThymeMessageResolverSpec.groovy
@@ -1,19 +1,24 @@
 package io.micronaut.views.thymeleaf
 
+//tag::imports[]
 import io.micronaut.context.ApplicationContext
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
 import spock.lang.Specification
-
+//end::imports[]
 class MicronautThymeMessageResolverSpec extends Specification {
 
     void "test template engine resolves micronaut message bundle"() {
         given:
         ApplicationContext ctx = ApplicationContext.run()
+        //tag::bean[]
         TemplateEngine templateEngine = ctx.getBean(TemplateEngine)
+        //end::bean[]
 
         when:
+        //tag::process[]
         String content = templateEngine.process("thymeleaf/sample", new Context())
+        //end::process[]
 
         then:
         content.contains("Sample Title")
