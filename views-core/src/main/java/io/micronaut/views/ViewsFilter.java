ec38f18bb007e173fd935b6950f15d9d437d87b6
Sergio del Amo <sergio.delamo@softamo.com>
2021-08-10 12:09:34 +0200

    BREAKING CHANGE: extract views decorator

--- views-core/src/main/java/io/micronaut/views/ViewsFilter.java
+++ views-core/src/main/java/io/micronaut/views/ViewsFilter.java
@@ -15,9 +15,8 @@
  */
 package io.micronaut.views;
 
-import io.micronaut.context.ApplicationContext;
 import io.micronaut.core.annotation.Nullable;
-import io.micronaut.inject.qualifiers.Qualifiers;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.annotation.AnnotationMetadata;
 import io.micronaut.core.annotation.NonNull;
@@ -32,14 +31,11 @@ import io.micronaut.http.filter.HttpServerFilter;
 import io.micronaut.http.filter.ServerFilterChain;
 import io.micronaut.http.filter.ServerFilterPhase;
 import io.micronaut.views.exceptions.ViewNotFoundException;
-import io.micronaut.views.model.ViewModelProcessor;
 import reactor.core.publisher.Flux;
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+
 import java.util.Optional;
 
 /**
@@ -54,23 +50,22 @@ import java.util.Optional;
 public class ViewsFilter implements HttpServerFilter {
     private static final Logger LOG = LoggerFactory.getLogger(ViewsFilter.class);
 
-    protected final ApplicationContext applicationContext;
-    protected final ViewsRendererLocator viewsRendererLocator;
     protected final ViewsResolver viewsResolver;
+    protected final ViewsRendererLocator viewsRendererLocator;
+    protected final ViewsModelDecorator viewsModelDecorator;
 
     /**
      * Constructor.
-     *
-     * @param applicationContext Application context
-     * @param viewsRendererLocator ViewsRenderer locator
      * @param viewsResolver Views Resolver
+     * @param viewsRendererLocator ViewRendererLocator
+     * @param viewsModelDecorator Views Model Decorator
      */
-    public ViewsFilter(ApplicationContext applicationContext,
+    public ViewsFilter(ViewsResolver viewsResolver,
                        ViewsRendererLocator viewsRendererLocator,
-                       ViewsResolver viewsResolver) {
-        this.applicationContext = applicationContext;
-        this.viewsRendererLocator = viewsRendererLocator;
+                       ViewsModelDecorator viewsModelDecorator) {
         this.viewsResolver = viewsResolver;
+        this.viewsRendererLocator = viewsRendererLocator;
+        this.viewsModelDecorator = viewsModelDecorator;
     }
 
     @Override
@@ -85,16 +80,11 @@ public class ViewsFilter implements HttpServerFilter {
             .switchMap(response -> {
                 Optional<String> optionalView = viewsResolver.resolveView(request, response);
                 if (!optionalView.isPresent()) {
+                    LOG.debug("no view found");
                     return Flux.just(response);
                 }
-                Optional<AnnotationMetadata> routeMatch = response.getAttribute(HttpAttributes.ROUTE_MATCH,
-                        AnnotationMetadata.class);
-                if (!routeMatch.isPresent()) {
-                    return Flux.just(response);
-                }
-                AnnotationMetadata route = routeMatch.get();
                 Object body = response.body();
-                MediaType type = resolveMediaType(route, body);
+                MediaType type = resolveMediaType(response, body);
                 String view = optionalView.get();
                 try {
                     Optional<ViewsRenderer> optionalViewsRenderer = viewsRendererLocator.resolveViewsRenderer(view,  type, body);
@@ -102,81 +92,35 @@ public class ViewsFilter implements HttpServerFilter {
                         LOG.debug("no view renderer found for media type: {}, ignoring", type.toString());
                         return Flux.just(response);
                     }
-                    ViewsRenderer viewsRenderer = optionalViewsRenderer.get();
-                    ModelAndView<?> modelAndView;
-                    if (body instanceof ModelAndView || body instanceof Map) {
-                        modelAndView = new ModelAndView<>(view, populateModel(request, viewsRenderer, body));
-                    } else {
-                        modelAndView = new ModelAndView<>(view, body);
-                    }
-                    enhanceModel(request, modelAndView);
-                    Writable writable = viewsRenderer.render(view, modelAndView.getModel().orElse(null), request);
+                    ModelAndView<?> modelAndView = new ModelAndView<>(view, body instanceof ModelAndView ? ((ModelAndView<?>) body).getModel().orElse(null) : body);
+                    viewsModelDecorator.decorate(request, modelAndView);
+                    Writable writable = optionalViewsRenderer.get().render(view, modelAndView.getModel().orElse(null), request);
                     response.contentType(type);
                     response.body(writable);
                     return Flux.just(response);
-
                 } catch (ViewNotFoundException e) {
                     return Flux.error(e);
                 }
             });
     }
 
+
     /**
      * Resolves the response content type for the matched route.
-     * @param route The matched route
-     * @param body The response body
+     * @param response HTTP response
+     * @param responseBody HTTP Response body
      * @return The resolved content type
      */
     @NonNull
-    protected MediaType resolveMediaType(@NonNull AnnotationMetadata route,
-                                         @Nullable Object body) {
+    protected MediaType resolveMediaType(@NonNull HttpResponse<?> response, @Nullable Object responseBody) {
+        Optional<AnnotationMetadata> routeMatch = response.getAttribute(HttpAttributes.ROUTE_MATCH,
+                AnnotationMetadata.class);
+        if (!routeMatch.isPresent()) {
+            return MediaType.APPLICATION_JSON_TYPE;
+        }
+        AnnotationMetadata route = routeMatch.get();
         return route.getValue(Produces.class, MediaType.class)
-                .orElse((route.getValue(View.class).isPresent() || body instanceof ModelAndView)
+                .orElse((route.getValue(View.class).isPresent() || responseBody instanceof ModelAndView)
                         ? MediaType.TEXT_HTML_TYPE : MediaType.APPLICATION_JSON_TYPE);
-    }
-
-    /**
-     * Resolves the model for the given response body and enhances the model with instances of {@link ViewModelProcessor}.
-     * @param request {@link HttpRequest} being processed
-     * @param viewsRenderer The Views rendered being used to render the view
-     * @param responseBody Response Body
-     * @return A model with the controllers response and enhanced with the decorators.
-     */
-    @SuppressWarnings("unused")
-    protected Map<String, Object> populateModel(HttpRequest<?> request, ViewsRenderer viewsRenderer, Object responseBody) {
-        return new HashMap<>(viewsRenderer.modelOf(resolveModel(responseBody)));
-    }
-
-    /**
-     * Resolves the model for the given response body. Subclasses can override to customize.
-     *
-     * @param responseBody Response body
-     * @return the model to be rendered
-     */
-    @SuppressWarnings({"WeakerAccess", "unchecked", "rawtypes"})
-    protected Object resolveModel(Object responseBody) {
-        if (responseBody instanceof ModelAndView) {
-            return ((ModelAndView) responseBody).getModel().orElse(null);
-        }
-        return responseBody;
-    }
-
-    /**
-     * Enhances a model by running it by all applicable ViewModelProcessors {@link ViewModelProcessor}.
-     *
-     * @param request      The http request this model relates to.
-     * @param modelAndView The ModelAndView to be enhanced.
-     */
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    protected void enhanceModel(HttpRequest<?> request, @NonNull ModelAndView<?> modelAndView) {
-        if (modelAndView.getModel().isPresent()) {
-            Collection<ViewModelProcessor> processors = applicationContext.getBeansOfType(ViewModelProcessor.class,
-                    Qualifiers.byTypeArguments(modelAndView.getModel().get().getClass())
-            );
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("located {} view model processors", processors.size());
-            }
-            processors.forEach(it -> it.process(request, modelAndView));
-        }
     }
 }
