1f74fb6b04f153edffd89b6082e97c48adae0e26
Edward Harman <jaq+github@ethelred.org>
2021-12-08 18:23:53 +0000

    add @Unroll

--- views-core/src/test/groovy/io/micronaut/views/ViewUtilsSpec.groovy
+++ views-core/src/test/groovy/io/micronaut/views/ViewUtilsSpec.groovy
@@ -1,8 +1,10 @@
 package io.micronaut.views
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class ViewUtilsSpec extends Specification {
+    @Unroll
     def "normalizeFile handles extensions with and without ."(path, extension, expected) {
         when:
         def actual = ViewUtils.normalizeFile(path, extension)
