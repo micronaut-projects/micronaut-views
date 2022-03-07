c5979c6a92c9e1de0155df050f0411c1fe7cfecd
Sergio del Amo <sergio.delamo@softamo.com>
2022-03-07 18:35:14 +0100

    test renderers support nullable request

---
+++ views-soy/src/test/groovy/io/micronaut/views/soy/SoyViewRenderNullableRequestSpec.groovy
@@ -0,0 +1,25 @@
+package io.micronaut.views.soy
+
+import io.micronaut.core.io.Writable
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Specification
+
+@MicronautTest(startApplication = false)
+class SoyViewRenderNullableRequestSpec extends Specification {
+
+    @Inject
+    SoySauceViewsRenderer<?> viewRenderer
+
+    void "views can be render with no request"() {
+        when:
+        Writable writeable = viewRenderer.render("sample.tim", ["username": "Tim"], null)
+        String result = new StringWriter().with {
+            writeable.writeTo(it)
+            it.toString()
+        }
+
+        then:
+        result.contains("username: <span>Tim</span>")
+    }
+}
