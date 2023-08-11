b304c55bf961d972aeb2a09084e693da08977460
Milenko Supic <supic82@gmail.com>
2023-08-11 09:55:01 +0200

    Added freemarker native tests

---
+++ test-suite-graal-freemarker/src/test/java/example/micronaut/FreemarkerTest.java
@@ -0,0 +1,66 @@
+package example.micronaut;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@MicronautTest
+public class FreemarkerTest {
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Test
+    void testHome() {
+        HttpRequest<?> request = HttpRequest.GET("/");
+        HttpResponse<String> response = client.toBlocking().exchange(request, String.class);
+        assertNotNull(response);
+        Optional<String> body = response.getBody();
+        assertTrue(body.isPresent());
+        assertTrue(body.get().contains("Title - Home"));
+        assertTrue(body.get().contains("<img src=\"images/micronaut_mini_copy_tm.svg\" width=\"400\"/>"));
+    }
+
+    @Test
+    void testLink() {
+        HttpRequest<?> request = HttpRequest.GET("/folder1/link1.html");
+        HttpResponse<String> response = client.toBlocking().exchange(request, String.class);
+        assertNotNull(response);
+        Optional<String> body = response.getBody();
+        assertTrue(body.isPresent());
+        assertTrue(body.get().contains("Title - Link"));
+        assertTrue(body.get().contains("<a href=\"https://micronaut.io/documentation.html\">Micronaut documentation</a>"));
+    }
+
+    @Test
+    void testFreemarker() {
+        HttpRequest<?> request = HttpRequest.GET("/views/freemarker");
+        HttpResponse<String> response = client.toBlocking().exchange(request, String.class);
+        assertNotNull(response);
+        Optional<String> body = response.getBody();
+        assertTrue(body.isPresent());
+        assertTrue(body.get().contains("Title - Freemarker"));
+        assertTrue(body.get().contains("username: <span>test-user</span>"));
+    }
+
+    @Test
+    void testFreemarkerPojoView() {
+        HttpRequest<?> request = HttpRequest.GET("/views/freemarker-view");
+        HttpResponse<String> response = client.toBlocking().exchange(request, String.class);
+        assertNotNull(response);
+        Optional<String> body = response.getBody();
+        assertTrue(body.isPresent());
+        assertTrue(body.get().contains("Title - Freemarker"));
+        assertTrue(body.get().contains("username: <span>test-user</span>"));
+    }
+}
