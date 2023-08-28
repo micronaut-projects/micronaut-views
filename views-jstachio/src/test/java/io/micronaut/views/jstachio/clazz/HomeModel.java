62f63246086e35b2fdeb9eb29985a8ade7fac6be
Sergio del Amo <sergio.delamo@softamo.com>
2023-08-28 13:30:59 +0200

    remove @Introspected

--- views-jstachio/src/test/java/io/micronaut/views/jstachio/clazz/HomeModel.java
+++ views-jstachio/src/test/java/io/micronaut/views/jstachio/clazz/HomeModel.java
@@ -2,10 +2,8 @@ package io.micronaut.views.jstachio.clazz;
 
 import io.jstach.jstache.JStache;
 import io.jstach.jstache.JStacheConfig;
-import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.annotation.Nullable;
 
-@Introspected
 @JStacheConfig(using = MicronautJStacheConfig.class)
 @JStache(path = "home")
 public record HomeModel(@Nullable String username, boolean loggedIn) {
--- views-jstachio/src/test/java/io/micronaut/views/jstachio/pkginfo/HomeModel.java
+++ views-jstachio/src/test/java/io/micronaut/views/jstachio/pkginfo/HomeModel.java
@@ -1,10 +1,8 @@
 package io.micronaut.views.jstachio.pkginfo;
 
 import io.jstach.jstache.JStache;
-import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.annotation.Nullable;
 
-@Introspected
 @JStache(path = "home")
 public record HomeModel(@Nullable String username, boolean loggedIn) {
 }
