c1794b1c123aa2cfacc456bceed6ccbdf5e5ee5f
Sergio del Amo <sergio.delamo@softamo.com>
2023-09-08 06:55:53 +0200

    remove @PendingFeature

--- views-jstachio/src/test/groovy/io/micronaut/views/jstachio/HtmlControllerSpec.groovy
+++ views-jstachio/src/test/groovy/io/micronaut/views/jstachio/HtmlControllerSpec.groovy
@@ -1,4 +1,3 @@
-
 package io.micronaut.views.jstachio
 
 import io.micronaut.context.annotation.Property
@@ -8,7 +7,6 @@ import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
-import spock.lang.PendingFeature
 import spock.lang.Specification
 
 @Property(name = "spec.name", value = "HtmlControllerSpec")
@@ -29,7 +27,6 @@ class HtmlControllerSpec extends Specification {
         "<!DOCTYPE html><html><head></head><body><h1>Sergio</h1></body></html>" == html
     }
 
-    @PendingFeature(reason = "https://github.com/micronaut-projects/micronaut-core/pull/9789")
     void "it is possible to return html without view and not handled by JStachioMessageBodyWriter"() {
         given:
         BlockingHttpClient client = httpClient.toBlocking()
