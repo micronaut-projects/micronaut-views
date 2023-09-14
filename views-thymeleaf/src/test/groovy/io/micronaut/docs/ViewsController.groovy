eab03d47b0816de41ef78d9a4a24837a45874dfd
Tim Yates <tim.yates@gmail.com>
2023-09-14 10:06:46 +0100

    Passing a List as the model to ModelAndView results in an IntrospectionException
    Reproducer for investigation and fixing

--- views-thymeleaf/src/test/groovy/io/micronaut/docs/ViewsController.groovy
+++ views-thymeleaf/src/test/groovy/io/micronaut/docs/ViewsController.groovy
@@ -56,6 +56,14 @@ class ViewsController {
     }
     //end::modelAndView[]
 
+    @Get("/modelAndViewWithList")
+    ModelAndView modelAndViewWithList() {
+        new ModelAndView(
+                "list",
+                List.of("a", "b", "c")
+        )
+    }
+
     @Get("/home")
     HttpResponse<Person> home() {
         HttpResponse.ok(new Person("sdelamo", true))
