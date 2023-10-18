ff324a4985e736a91f64ed35b48aded498b12d27
Sergio del Amo <sergio.delamo@softamo.com>
2023-10-18 17:41:50 +0200

    Update views-fieldset/src/main/java/io/micronaut/views/fields/InputPasswordFormElement.java
    Co-authored-by: Tim Yates <tim.yates@gmail.com>

--- views-fieldset/src/main/java/io/micronaut/views/fields/InputPasswordFormElement.java
+++ views-fieldset/src/main/java/io/micronaut/views/fields/InputPasswordFormElement.java
@@ -54,7 +54,7 @@ public class InputPasswordFormElement implements FormElement, FormElementAttribu
      * The maximum string length that the user can enter into the password input.
      */
     @Nullable
-    private Integer maxLength;
+    private final Integer maxLength;
 
     /**
      * The minimum string length that the user can enter into the password input.
