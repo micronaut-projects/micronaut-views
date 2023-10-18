d588dd28c97bb35b11b8980fac78d0a09652ce36
Sergio del Amo <sergio.delamo@softamo.com>
2023-10-18 17:40:06 +0200

    Update views-fieldset/src/main/java/io/micronaut/views/fields/InputUrlFormElement.java
    Co-authored-by: Tim Yates <tim.yates@gmail.com>

--- views-fieldset/src/main/java/io/micronaut/views/fields/InputUrlFormElement.java
+++ views-fieldset/src/main/java/io/micronaut/views/fields/InputUrlFormElement.java
@@ -48,7 +48,7 @@ public class InputUrlFormElement implements FormElement, GlobalAttributes, FormE
      * The maximum string length that the user can enter into the text input.
      */
     @Nullable
-    private Integer maxLength;
+    private final Integer maxLength;
 
     /**
      * The minimum string length that the user can enter into the text input.
