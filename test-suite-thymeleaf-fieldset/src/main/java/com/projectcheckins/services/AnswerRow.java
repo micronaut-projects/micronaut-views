5d00cb0976df56c5360d0fdb5f420cdc92130369
Sergio del Amo <sergio.delamo@softamo.com>
2023-10-30 09:19:17 +0100

    foo

--- test-suite-thymeleaf-fieldset/src/main/java/com/projectcheckins/services/AnswerRow.java
+++
@@ -1,9 +0,0 @@
-package com.projectcheckins.services;
-
-import io.micronaut.core.annotation.Introspected;
-import io.micronaut.core.annotation.NonNull;
-import jakarta.validation.constraints.NotBlank;
-
-@Introspected
-public record AnswerRow(@NonNull @NotBlank String answer) {
-}
