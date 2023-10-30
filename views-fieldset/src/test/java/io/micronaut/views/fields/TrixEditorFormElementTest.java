7a869690ae3a6e2447d6cc6a2cac4958fd8f0193
Sergio del Amo <sergio.delamo@softamo.com>
2023-10-30 11:19:04 +0100

    trix editor (#625)
    see: https://github.com/basecamp/trix

--- views-fieldset/src/test/java/io/micronaut/views/fields/TrixEditorFormElementTest.java
+++ views-fieldset/src/test/java/io/micronaut/views/fields/TrixEditorFormElementTest.java
@@ -6,47 +6,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class TextAreaFormElementTest {
+class TrixEditorFormElementTest {
 
     @Test
-    void textareaHasABuilderApi() {
+    void trixeditorHasABuilderApi() {
         String name = "w3review";
         String id = "w3reviewid";
-        Integer rows = 4;
-        Integer cols = 50;
         String content = "At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.";
-        TextareaFormElement formElement = TextareaFormElement.builder()
+        TrixEditorFormElement formElement = TrixEditorFormElement.builder()
             .name(name)
             .id(id)
-            .rows(rows)
-            .cols(cols)
             .value(content)
             .build();
-        assetTextAreaFormElement(name, id, rows, cols, content, formElement);
+        assetTextAreaFormElement(name, id, content, formElement);
 
-        BeanIntrospection<TextareaFormElement> introspection = BeanIntrospection.getIntrospection(TextareaFormElement.class);
-        BeanIntrospection.Builder<TextareaFormElement> builder = introspection.builder();
-        TextareaFormElement form = builder
+        BeanIntrospection<TrixEditorFormElement> introspection = BeanIntrospection.getIntrospection(TrixEditorFormElement.class);
+        BeanIntrospection.Builder<TrixEditorFormElement> builder = introspection.builder();
+        TrixEditorFormElement form = builder
             .with("name", name)
             .with("id", id)
-            .with("rows", rows)
-            .with("cols", cols)
             .with("value", content)
             .build();
-        assetTextAreaFormElement(name, id, rows, cols, content, formElement);
+        assetTextAreaFormElement(name, id, content, formElement);
     }
 
     private void assetTextAreaFormElement( String name,
                                            String id,
-                                           Integer rows,
-                                           Integer cols,
                                            String content,
-                                           TextareaFormElement formElement) {
+                                           TrixEditorFormElement formElement) {
         assertNotNull(formElement);
         assertEquals(name, formElement.name());
         assertEquals(id, formElement.id());
-        assertEquals(rows, formElement.rows());
-        assertEquals(cols, formElement.cols());
         assertEquals(content, formElement.value());
     }
 }
