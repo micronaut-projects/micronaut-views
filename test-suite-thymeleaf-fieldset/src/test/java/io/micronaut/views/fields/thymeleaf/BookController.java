306502d20ab8cd1b5daf5b1d87c169cd8b20d711
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-07 10:38:57 +0100

    add test

--- test-suite-thymeleaf-fieldset/src/test/java/io/micronaut/views/fields/thymeleaf/BookController.java
+++ test-suite-thymeleaf-fieldset/src/test/java/io/micronaut/views/fields/thymeleaf/BookController.java
@@ -1,5 +1,6 @@
 package io.micronaut.views.fields.thymeleaf;
 
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
@@ -9,16 +10,23 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.uri.UriBuilder;
+import io.micronaut.views.ModelAndView;
 import io.micronaut.views.View;
+import io.micronaut.views.fields.Form;
 import io.micronaut.views.fields.FormGenerator;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
-
+import io.micronaut.http.annotation.Error;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 //tag::clazz[]
 @Controller("/books")
 class BookController {
+    public static final String CONTROLLER_PATH = "/books";
+    public static final String SAVE_PATH = "/save";
+    public static final String FORM = "form";
 
     private final FormGenerator formGenerator;
     private final BookRepository bookRepository;
@@ -33,8 +41,8 @@ class BookController {
     @View("/books/create.html")
     @Get("/create")
     Map<String, Object> create() {
-        return Collections.singletonMap("form",
-                formGenerator.generate("/books/save", BookSave.class));
+        return Collections.singletonMap(FORM,
+                formGenerator.generate(CONTROLLER_PATH + SAVE_PATH, BookSave.class));
     }
 
     @Produces(MediaType.TEXT_HTML)
@@ -46,10 +54,24 @@ class BookController {
 
     @Produces(MediaType.TEXT_HTML)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Post("/save")
+    @Post(SAVE_PATH)
     HttpResponse<?> save(@Valid @Body BookSave bookSave) {
         bookRepository.save(new Book(null, bookSave.title(), bookSave.pages()));
         return HttpResponse.seeOther(UriBuilder.of("/books").path("list").build());
+    }
+
+    @Error(exception = ConstraintViolationException.class)
+    public HttpResponse<?> onConstraintViolationException(HttpRequest<?> request, ConstraintViolationException ex) {
+        if (request.getPath().equals(CONTROLLER_PATH + SAVE_PATH)) {
+            Optional<BookSave> bookSaveOptional = request.getBody(BookSave.class);
+            if (bookSaveOptional.isPresent()) {
+                Form form = formGenerator.generate(CONTROLLER_PATH + SAVE_PATH, bookSaveOptional.get(), ex);
+                ModelAndView<Map<String, Object>> body = new ModelAndView<>("/books/create.html",
+                        Collections.singletonMap(FORM, form));
+                return HttpResponse.unprocessableEntity().body(body);
+            }
+        }
+        return HttpResponse.serverError();
     }
 }
 //end::clazz[]
--- test-suite-thymeleaf-fieldset/src/test/java/io/micronaut/views/fields/thymeleaf/BookControllerTest.java
+++ test-suite-thymeleaf-fieldset/src/test/java/io/micronaut/views/fields/thymeleaf/BookControllerTest.java
@@ -1,17 +1,20 @@
 package io.micronaut.views.fields.thymeleaf;
 
 import io.micronaut.context.annotation.Property;
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.BlockingHttpClient;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static io.micronaut.views.fields.thymeleaf.TestUtils.formPost;
 import static io.micronaut.views.fields.thymeleaf.TestUtils.htmlGet;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Property(name = "datasources.default.password", value = "")
 @Property(name = "datasources.default.dialect", value = "H2")
@@ -48,5 +51,30 @@ class BookControllerTest {
         html = assertDoesNotThrow(() -> client.retrieve(formPost("/books/save", "title=Building Microservices&pages=120")));
         assertTrue(html.contains("<!DOCTYPE html>"));
         assertTrue(html.contains("<li><span>Building Microservices</span><span>120</span></li>"));
+    }
+
+    @Test
+    void formIsValidated(@Client("/") HttpClient httpClient) {
+        BlockingHttpClient client = httpClient.toBlocking();
+        Argument okResponse = Argument.of(String.class);
+        Argument failureResponse = Argument.of(String.class);
+        HttpClientResponseException e = assertThrows(HttpClientResponseException.class, () -> client.retrieve(formPost("/books/save", "title=Building Microservices&pages=0"), okResponse, failureResponse));
+        assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, e.getStatus());
+        Optional<String> htmlOptional = e.getResponse().getBody(String.class);
+        assertTrue(htmlOptional.isPresent());
+        String html = htmlOptional.get();
+        assertTrue(html.contains("""
+                <form action="/books/save" method="post">\
+                <div class="mb-3">\
+                <label for="title" class="form-label">Title</label>\
+                <input type="text" name="title" value="Building Microservices" id="title" minlength="2" maxlength="255" class="form-control" required="required"/>\
+                </div>\
+                <div class="mb-3">\
+                <label for="pages" class="form-label">Pages</label><input type="number" name="pages" value="0" id="pages" min="1" max="21450" class="form-control is-invalid" aria-describedby="pagesValidationServerFeedback" required="required"/>\
+                <div id="pagesValidationServerFeedback" class="invalid-feedback">must be greater than or equal to 1</div>\
+                </div>\
+                <input type="submit" value="Submit" class="btn btn-primary"/>\
+                </form>"""));
+
     }
 }
