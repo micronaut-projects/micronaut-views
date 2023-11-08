9565f1b4ab7ebf69a659f4d45fe0660851c7ca02
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-08 13:33:19 +0100

    remove context

--- views-fieldset/build.gradle.kts
+++ views-fieldset/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 dependencies {
     annotationProcessor(mnValidation.micronaut.validation.processor)
     implementation(mnValidation.micronaut.validation)
-    implementation(mn.micronaut.context)
     testAnnotationProcessor(mnValidation.micronaut.validation.processor)
     testImplementation(mnValidation.micronaut.validation)
 
