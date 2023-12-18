80cbaae7c8d116804242bdbc93784956e2e73c9e
Sergio del Amo <sergio.delamo@softamo.com>
2023-12-18 22:32:43 +0100

    remove OrderUtil::sort

--- views-fieldset/src/test/java/io/micronaut/views/fields/formelementresolvers/FormElementResolverTest.java
+++ views-fieldset/src/test/java/io/micronaut/views/fields/formelementresolvers/FormElementResolverTest.java
@@ -1,7 +1,6 @@
 package io.micronaut.views.fields.formelementresolvers;
 
 import io.micronaut.context.BeanContext;
-import io.micronaut.core.order.OrderUtil;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
@@ -20,7 +19,6 @@ class FormElementResolverTest {
     @Test
     void defaultFormElementResolverFirst() {
         List<FormElementResolver> resolvers = new ArrayList<>(beanContext.getBeansOfType(FormElementResolver.class));
-        OrderUtil.sort(resolvers);
         assertEquals(3, resolvers.size());
         assertTrue(resolvers.get(0) instanceof CompositeFormElementResolver);
         assertTrue(resolvers.get(1) instanceof DefaultFormElementResolver);
