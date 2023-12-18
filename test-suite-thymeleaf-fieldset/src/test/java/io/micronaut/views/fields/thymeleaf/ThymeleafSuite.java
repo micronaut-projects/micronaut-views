4a77c063b4565ea95e5ba1dacdf919ef41741c90
Sergio del Amo <sergio.delamo@softamo.com>
2023-12-18 13:35:22 +0100

    remove @InlcudNamePatterns

--- test-suite-thymeleaf-fieldset/src/test/java/io/micronaut/views/fields/thymeleaf/ThymeleafSuite.java
+++ test-suite-thymeleaf-fieldset/src/test/java/io/micronaut/views/fields/thymeleaf/ThymeleafSuite.java
@@ -1,6 +1,5 @@
 package io.micronaut.views.fields.thymeleaf;
 
-import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
@@ -9,7 +8,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @SelectPackages({
         "io.micronaut.views.fields.tck",
 })
-@IncludeClassNamePatterns("io.micronaut.views.fields.tck.FormCompletedFileUploadRenderTest")
 @SuiteDisplayName("Fieldset TCK for Thymeleaf")
 class ThymeleafSuite {
 }
