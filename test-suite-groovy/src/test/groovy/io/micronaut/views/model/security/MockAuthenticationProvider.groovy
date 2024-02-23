27f65522926dd9111acb81fabc6cc58ae603e559
Tim Yates <tim.yates@gmail.com>
2024-02-23 16:46:36 +0000

    test: Remove deprecated AuthenticationProvider usage in test (#720)

--- test-suite-groovy/src/test/groovy/io/micronaut/views/model/security/MockAuthenticationProvider.groovy
+++ test-suite-groovy/src/test/groovy/io/micronaut/views/model/security/MockAuthenticationProvider.groovy
@@ -1,24 +1,17 @@
-package io.micronaut.views.model.security;
+package io.micronaut.views.model.security
 
-import io.micronaut.core.annotation.Nullable;
-import io.micronaut.http.HttpRequest;
-import io.micronaut.security.authentication.AuthenticationProvider;
-import io.micronaut.security.authentication.AuthenticationRequest;
-import io.micronaut.security.authentication.AuthenticationResponse;
-import jakarta.inject.Singleton;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
-
-import java.util.Collections;
+import io.micronaut.core.annotation.Nullable
+import io.micronaut.http.HttpRequest
+import io.micronaut.security.authentication.AuthenticationRequest
+import io.micronaut.security.authentication.AuthenticationResponse
+import io.micronaut.security.authentication.provider.HttpRequestAuthenticationProvider
+import jakarta.inject.Singleton
 
 @Singleton
-public class MockAuthenticationProvider implements AuthenticationProvider<HttpRequest<?>> {
+class MockAuthenticationProvider<B> implements HttpRequestAuthenticationProvider<B> {
+
     @Override
-    public Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
-        return Flux.create(emitter -> {
-            emitter.next(AuthenticationResponse.success(authenticationRequest.getIdentity().toString(), Collections.singletonMap("email", "john@email.com")));
-            emitter.complete();
-        }, FluxSink.OverflowStrategy.ERROR);
+    AuthenticationResponse authenticate(@Nullable HttpRequest<B> httpRequest, AuthenticationRequest<String, String> authenticationRequest) {
+        AuthenticationResponse.success(authenticationRequest.getIdentity().toString(), [email: "john@email.com"])
     }
 }
