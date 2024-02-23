fdafebb7d14921f595fb4275524ee6c038fec185
Tim Yates <tim.yates@gmail.com>
2024-02-23 16:11:16 +0000

    test: Remove deprecated AuthenticationProvider usage in test

--- test-suite-kotlin/src/test/kotlin/io/micronaut/views/model/security/MockAuthenticationProvider.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/views/model/security/MockAuthenticationProvider.kt
@@ -3,15 +3,14 @@ package io.micronaut.views.model.security
 import io.micronaut.http.HttpRequest
 import io.micronaut.security.authentication.AuthenticationRequest
 import io.micronaut.security.authentication.AuthenticationResponse
-import io.micronaut.security.authentication.provider.AuthenticationProvider
+import io.micronaut.security.authentication.provider.HttpRequestAuthenticationProvider
 import jakarta.inject.Singleton
 
 @Singleton
-class MockAuthenticationProvider : AuthenticationProvider<HttpRequest<*>, Any, Any> {
+class MockAuthenticationProvider<B> : HttpRequestAuthenticationProvider<B> {
 
-    override fun authenticate(requestContext: HttpRequest<*>, authRequest: AuthenticationRequest<Any, Any>): AuthenticationResponse =
-        AuthenticationResponse.success(
-            authRequest.identity.toString(),
-            mapOf("email" to "john@email.com")
-        )
+    override fun authenticate(
+        requestContext: HttpRequest<B>,
+        authRequest: AuthenticationRequest<String, String>
+    ): AuthenticationResponse = AuthenticationResponse.success(authRequest.identity, mapOf("email" to "john@email.com"))
 }
--- test-suite/src/test/java/io/micronaut/views/model/security/MockAuthenticationProvider.java
+++ test-suite/src/test/java/io/micronaut/views/model/security/MockAuthenticationProvider.java
@@ -2,22 +2,18 @@ package io.micronaut.views.model.security;
 
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpRequest;
-import io.micronaut.security.authentication.AuthenticationProvider;
 import io.micronaut.security.authentication.AuthenticationRequest;
 import io.micronaut.security.authentication.AuthenticationResponse;
-import reactor.core.publisher.FluxSink;
-import reactor.core.publisher.Flux;
-import org.reactivestreams.Publisher;
+import io.micronaut.security.authentication.provider.HttpRequestAuthenticationProvider;
 import jakarta.inject.Singleton;
+
 import java.util.Collections;
 
 @Singleton
-public class MockAuthenticationProvider implements AuthenticationProvider<HttpRequest<?>> {
+public class MockAuthenticationProvider<B> implements HttpRequestAuthenticationProvider<B> {
+
     @Override
-    public Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
-        return Flux.create(emitter -> {
-            emitter.next(AuthenticationResponse.success(authenticationRequest.getIdentity().toString(), Collections.singletonMap("email", "john@email.com")));
-            emitter.complete();
-        }, FluxSink.OverflowStrategy.ERROR);
+    public AuthenticationResponse authenticate(@Nullable HttpRequest<B> httpRequest, AuthenticationRequest<String, String> authenticationRequest) {
+        return AuthenticationResponse.success(authenticationRequest.getIdentity(), Collections.singletonMap("email", "john@email.com"));
     }
 }
