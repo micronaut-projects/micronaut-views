d7238b8aa24662a3b5fe0c7a0278c3a6c7706f5a
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-13 11:17:52 +0100

    Interfaces should not solely consist of constants

--- views-htmx/src/main/java/io/micronaut/views/htmx/http/HtmxResponseHeaders.java
+++ views-htmx/src/main/java/io/micronaut/views/htmx/http/HtmxResponseHeaders.java
@@ -21,81 +21,84 @@ package io.micronaut.views.htmx.http;
  * @author Sergio del Amo
  * @since 6.0.0
  */
-public interface HtmxResponseHeaders {
+public final class HtmxResponseHeaders {
     /**
      * Allows you to do a client-side redirect that does not do a full page reload.
      *
      * @see <a href="https://htmx.org/headers/hx-location/">HX-Location</a>
      */
-    String HX_LOCATION = "HX-Location";
+    public static final String HX_LOCATION = "HX-Location";
 
     /**
      * Allows you to replace the current URL in the location bar.
      *
      * @see <a href="https://htmx.org/headers/hx-replace-url/">HX-Replace-Url</a>
      */
-    String HX_REPLACE_URL = "HX-Replace-Url";
+    public static final String HX_REPLACE_URL = "HX-Replace-Url";
 
     /**
      * Pushes a new url into the history stack.
      *
      * @see <a href="https://htmx.org/headers/hx-push-url">HX-Push</a>
      */
-    String HX_PUSH_URL = "HX-Push-Url";
+    public static final String HX_PUSH_URL = "HX-Push-Url";
 
     /**
      * Can be used to do a client-side redirect to a new location.
      *
      * @see <a href="https://htmx.org/reference/#response_headers">HX-Redirect</a>
      */
-    String HX_REDIRECT = "HX-Redirect";
+    public static final String HX_REDIRECT = "HX-Redirect";
 
     /**
      * Can be used to do a full refresh of the page on the client-side.
      *
      * @see <a href="https://htmx.org/reference/#response_headers">HX-Refresh</a>
      */
-    String HX_REFRESH = "HX-Refresh";
+    public static final String HX_REFRESH = "HX-Refresh";
 
     /**
      * A CSS selector that updates the target of the content update to a different element on the page.
      *
      * @see <a href="https://htmx.org/reference/#response_headers">HX-Retarget</a>
      */
-    String HX_RETARGET = "HX-Retarget";
+    public static final String HX_RETARGET = "HX-Retarget";
 
     /**
      * A CSS selector that allows you to choose which part of the response is used to be swapped in.
      *
      * @see <a href="https://htmx.org/reference/#response_headers">HX-Reselect</a>
      */
-    String HX_RESELECT = "HX-Reselect";
+    public static final String HX_RESELECT = "HX-Reselect";
 
     /**
      * Can be used to trigger client side events.
      *
      * @see <a href="https://htmx.org/headers/hx-trigger/">HX-Trigger</a>
      */
-    String HX_TRIGGER = "HX-Trigger";
+    public static final String HX_TRIGGER = "HX-Trigger";
 
     /**
      * Can be used to trigger client side events after the <a href="https://htmx.org/docs/#request-operations">settling step</a>.
      *
      * @see <a href="https://htmx.org/headers/hx-trigger/">HX-Trigger-After-Settle</a>
      */
-    String HX_TRIGGER_AFTER_SETTLE = "HX-Trigger-After-Settle";
+    public static final String HX_TRIGGER_AFTER_SETTLE = "HX-Trigger-After-Settle";
 
     /**
      * Can be used to trigger client side events after the <a href="https://htmx.org/docs/#request-operations">swap step</a>.
      *
      * @see <a href="https://htmx.org/headers/hx-trigger/">HX-Trigger-After-Settle</a>
      */
-    String HX_TRIGGER_AFTER_SWAP = "HX-Trigger-After-Swap";
+    public static final String HX_TRIGGER_AFTER_SWAP = "HX-Trigger-After-Swap";
 
     /**
      * Allows you to specify how the response will be swapped.
      *
      * @see <a href="https://htmx.org/reference/#response_headers">HX-Reswap</a>
      */
-    String HX_RESWAP = "HX-Reswap";
+    public static final String HX_RESWAP = "HX-Reswap";
+
+    private HtmxResponseHeaders() {
+    }
 }
