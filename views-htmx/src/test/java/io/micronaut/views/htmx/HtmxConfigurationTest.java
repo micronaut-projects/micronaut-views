4d277983b0b22dff7e941c46e120d4b90259275b
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-14 07:56:26 +0100

    remove duplicated test

--- views-htmx/src/test/java/io/micronaut/views/htmx/HtmxConfigurationTest.java
+++ views-htmx/src/test/java/io/micronaut/views/htmx/HtmxConfigurationTest.java
@@ -24,16 +24,4 @@ class HtmxConfigurationTest {
             assertFalse(ctx.containsBean(HtmxConfiguration.class));
         }
     }
-    @Test
-    void canBeDisabledViaProperties() {
-        try (ApplicationContext ctx = ApplicationContext.run(Map.of("micronaut.views.htmx.enabled", "false"))) {
-            assertFalse(ctx.containsBean(HtmxConfiguration.class));
-        }
-    }
-    @Test
-    void canBeDisabledViaProperties() {
-        try (ApplicationContext ctx = ApplicationContext.run(Map.of("micronaut.views.htmx.enabled", "false"))) {
-            assertFalse(ctx.containsBean(HtmxConfiguration.class));
-        }
-    }
 }
