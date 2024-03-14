b0be70f154d451c7aa65c8f38edb2be8088f5c67
Sergio del Amo <sergio.delamo@softamo.com>
2024-03-14 07:51:36 +0100

    Update views-htmx/src/test/java/io/micronaut/views/htmx/http/TodoItemMapper.java
    Co-authored-by: Tim Yates <tim.yates@gmail.com>

--- views-htmx/src/test/java/io/micronaut/views/htmx/http/TodoItemMapper.java
+++ views-htmx/src/test/java/io/micronaut/views/htmx/http/TodoItemMapper.java
@@ -4,7 +4,7 @@ import io.micronaut.context.annotation.Mapper;
 import io.micronaut.context.annotation.Property;
 import jakarta.inject.Singleton;
 
-@Property(name = "spec.name", value = "HtmxRequestHeadersTypedRequestArgumentBinderTest")
+@Requires(property = "spec.name", value = "HtmxRequestHeadersTypedRequestArgumentBinderTest")
 @Singleton
 public interface TodoItemMapper {
     @Mapper.Mapping(to = "completed", from = "#{false}")
