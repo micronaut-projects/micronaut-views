4fa3ca53707f90f09060eb5c3d0ce2a151fa3a72
Mike Hearn <michael.hearn@oracle.com>
2024-04-17 14:51:43 +0200

    React Renderer: update client.preact.js to new global symbol name.

--- views-react/src/test/js/client.preact.js
+++ views-react/src/test/js/client.preact.js
@@ -1,8 +1,8 @@
 import {h, render} from 'preact'
 
-const pageComponentName = __micronaut_boot.rootComponent;
+const pageComponentName = Micronaut.rootComponent;
 
 import(`./components/${pageComponentName}.js`).then(module => {
     const PageComponent = module[pageComponentName]
-    render(h(PageComponent, __micronaut_boot.rootProps, null), document)
+    render(h(PageComponent, Micronaut.rootProps, null), document)
 })
