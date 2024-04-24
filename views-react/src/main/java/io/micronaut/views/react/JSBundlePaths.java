b0e92cc8fa79cb89d5357310fc6c7df741e7491d
Mike Hearn <michael.hearn@oracle.com>
2024-04-24 18:52:56 +0200

    React Renderer: add license headers

--- views-react/src/main/java/io/micronaut/views/react/JSBundlePaths.java
+++ views-react/src/main/java/io/micronaut/views/react/JSBundlePaths.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.views.react;
 
 import io.micronaut.views.ViewsConfiguration;
@@ -40,8 +55,9 @@ class JSBundlePaths {
             var now = System.currentTimeMillis();
             // Skip the check if it was done very recently. This makes the check disappear from
             // profiles under load.
-            if (now - lastModificationCheckTime <= 500)
+            if (now - lastModificationCheckTime <= 500) {
                 return false;
+            }
             lastModificationCheckTime = now;
 
             FileTime time = Files.getLastModifiedTime(bundlePath);
