19cf6aed5cde2d58d1189e8dfa863cef65b0d231
Tim Yates <tim.yates@gmail.com>
2024-05-14 09:49:07 +0100

    test more paths

--- views-core/src/test/groovy/io/micronaut/views/MediaTypeParseSpec.groovy
+++ views-core/src/test/groovy/io/micronaut/views/MediaTypeParseSpec.groovy
@@ -5,12 +5,14 @@ import spock.lang.Specification
 
 class MediaTypeParseSpec extends Specification {
 
-    void "string based media type extraction works as expected for #type"() {
+    void "string based media type extraction works as expected for '#type'"() {
         expect:
         DefaultModelAndViewRenderer.mediaTypeName(type) == expected
 
         where:
         type                                     || expected
+        null                                     || null
+        ''                                       || ''
         'application/json'                       || MediaType.APPLICATION_JSON
         'application/json;charset=utf-8'         || MediaType.APPLICATION_JSON
         'application/json; charset=utf-8'        || MediaType.APPLICATION_JSON
