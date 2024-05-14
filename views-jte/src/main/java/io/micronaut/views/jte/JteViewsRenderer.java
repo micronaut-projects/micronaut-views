a30de11aae4a933c2b0d5d670ba70c9e3c8c145b
Tim Yates <tim.yates@gmail.com>
2024-05-14 09:44:27 +0100

    Checkstyle

--- views-jte/src/main/java/io/micronaut/views/jte/JteViewsRenderer.java
+++ views-jte/src/main/java/io/micronaut/views/jte/JteViewsRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2023 original authors
+ * Copyright 2017-2024 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.Arrays;
 
