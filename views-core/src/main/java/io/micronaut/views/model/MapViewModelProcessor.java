d15e09247b6832a1b98f8c0af2e81edd215fbb73
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-30 09:39:56 +0100

    use Map on the left hand side

--- views-core/src/main/java/io/micronaut/views/model/MapViewModelProcessor.java
+++ views-core/src/main/java/io/micronaut/views/model/MapViewModelProcessor.java
@@ -29,14 +29,14 @@ public abstract class MapViewModelProcessor implements ViewModelProcessor<Map<St
     @Override
     public void process(@NonNull HttpRequest<?> request, @NonNull ModelAndView<Map<String, Object>> modelAndView) {
         Map<String, Object> viewModel = modelAndView.getModel().orElseGet(() -> {
-            final HashMap<String, Object> newModel = new HashMap<>(1);
+            final Map<String, Object> newModel = new HashMap<>(1);
             modelAndView.setModel(newModel);
             return newModel;
         });
         try {
             populateModel(request, viewModel);
         } catch (UnsupportedOperationException ex) {
-            final HashMap<String, Object> modifiableModel = new HashMap<>(viewModel);
+            final Map<String, Object> modifiableModel = new HashMap<>(viewModel);
             populateModel(request, modifiableModel);
             modelAndView.setModel(modifiableModel);
         }
